344f5bcad46a19c46e367c6b38548a09c52f5a73
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-23 22:17:16 +0300

    Add files via upload

---
+++ SwipeToPay/SwipeToPay/SwipeToPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeToPayApp.swift
+//  SwipeToPay
+//
+//  Created by Berkan Aydın on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwipeToPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
